1fdf1d52c4e8d5ad1a5272bf7e4bda3705495441
Gaojunlong <1721980284@qq.com>
2019-03-28 19:30:23 +0800

    gao

---
+++ Gaojunlong/阿超的四则运算/CalculatorTest/UnitTest.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+namespace CalculatorTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+       public void TestMethod1()
+        {
+            Calculator calculator = new Calculator();
+            int fhlen = calculator.Genelment();
+            string _ = calculator.Gensign(fhlen);
+            int result = calculator.Genresult();
+            string timu = calculator.Gettimu();
+            int isok = calculator.Getisok();
+        }
+    }
+}
