12245677be24df3ac9ae4c085cb97e1a5df1bab8
lynb1314 <illl1314@foxmail.com>
2019-03-28 20:25:14 +0800

    First Commit

---
+++ lynb1314/Calculator/CalculatorTests/ProgramTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void Make_FormulaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CalculateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
