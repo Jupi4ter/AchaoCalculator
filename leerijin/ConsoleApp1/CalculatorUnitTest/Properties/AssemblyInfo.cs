db1caec21f6755779088e5698b3e5dc2e5eae980
leerijin <1053341523@qq.com>
2019-03-28 21:33:12 +0800

    四则运算及其单元测试

---
+++ leerijin/ConsoleApp1/CalculatorUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("82ea0248-4ba8-4ad9-aa01-97f615e7c878")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ leerijin/ConsoleApp1/CalculatorUnitTest/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data;
+using ConsoleApp1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var seed = Guid.NewGuid().GetHashCode();
+            var aRandom = new System.Random(seed);    //设置随机种子
+            int num_s = aRandom.Next(2, 4);       //产生符号个数
+            int num = num_s + 1;
+            string equation = "";
+            //将产生的数字和符号存储在字符串中
+            for (int i = 0; i < (num + num_s); i++)
+            {
+                if (i % 2 == 0)
+                {
+                    equation += aRandom.Next(0, 101).ToString();
+                }
+                else
+                {
+                    equation += Program.Getop(aRandom.Next(0, 4)).ToString();
+                }
+            }
+            string test1 = "36*2-50";
+            string test2 = "28/4*3";
+            string date = equation;
+            DataTable dt = new DataTable();
+            string really_data = dt.Compute(equation, "false").ToString();
+            Assert.AreEqual(Program.Calculate(test1), "22");
+            Assert.AreEqual(Program.Calculate(test2), "21");
+            Assert.AreEqual(Program.Calculate(equation), really_data);
+        }
+    }
+}
