3053fe097223e0d9693c4c68838577a3bf0ae415
HuangJieH <1583775175@qq.com>
2019-03-29 13:27:28 +0800

    HJH

---
+++ HuangJieH/ConsoleApp11/ConsoleApp11Tests1/ProgramTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp11;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void GreatCalculationTest()
+        {
+            string a = "3*15";
+            string b = Program.Calculation(a);
+            string c = "3*15=45";
+            Assert.AreEqual(b,c);
+         }
+    }
+}
