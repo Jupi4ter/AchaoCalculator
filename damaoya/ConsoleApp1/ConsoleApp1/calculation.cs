22e42eba7415c29fe309209a629141179c7e4faa
unknown <1206467925@qq.com>
2019-03-29 14:21:45 +0800

    damaoya

---
+++ damaoya/ConsoleApp1/ConsoleApp1/calculation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class calculation
+    {
+        internal string GetProblem(int r, ref int[] result, ref int[] v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
